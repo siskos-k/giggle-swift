362e7f45e7db4b189f62c99f9d85c8fccc581c87
ntua-el19887 <konstantinos.siscos@gmail.com>
2024-12-24 00:14:57 +0200

    profile Screen + functionality

--- Views/ProfileView.swift
+++ Views/ProfileView.swift
@@ -14,13 +14,48 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
-                
-            }
+                if let user = viewModel.user{
+                    
+                    //Avatar
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .frame(width: 125, height: 125)
+                    
+                    //info
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Name:").bold()
+                            Text(user.name)
+                        }
+                        .padding()
+                        HStack {
+                            Text("Email:").bold()
+                            Text(user.email)
+                        }
+                        .padding()
+                        HStack {
+                            Text("Member Since:").bold()
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                        }
+                        .padding()
+                    }
+                    
+                    Button("Log Out"){viewModel.logOut()}
+                }
+                else {
+                    Text("Loading Profile")
+                }
+                }
             .navigationTitle("Profile")
-
         }
-    }
+        .onAppear{
+            viewModel.fetchUser()
+        }
 }
+    }
+       
 
 #Preview {
     ProfileView()
